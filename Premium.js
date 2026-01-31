74f184008ce347e87c7e2afb70781e31b47df25c
HeriTezzRorw <235550888+heriekoprasetyo56-boop@users.noreply.github.com>
2026-02-01 00:28:49 +0700

    Delete Premium.js

--- Premium.js
+++
@@ -1,7 +0,0 @@
-const premiumList = require('../premium.json'); // Import dari luar folder premium
-
-const isPremium = (userId) => {
-  return premiumList.includes(userId);
-};
-
-module.exports = { isPremium };
